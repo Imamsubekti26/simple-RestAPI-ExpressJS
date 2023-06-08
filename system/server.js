193d8eb1db58a3c647204370cc46b33b00bf3ccb
Imamsubekti26 <imam0subekti@gmail.com>
2023-06-08 17:35:43 +0700

    add cors

--- system/server.js
+++ system/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const userController = require("../controllers/userController.js");
 
 require("dotenv").config();
@@ -6,6 +7,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/api/users", userController.index);
 app.get("/api/users/:id", userController.find);
@@ -14,5 +16,5 @@ app.put("/api/users/:id", userController.update);
 app.delete("/api/users/:id", userController.deleteData);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`app listening on port ${port}`);
 });
